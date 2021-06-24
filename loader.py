58abd348bd67ebeffd572bf5a55f0a6bb65e8ee9
Akimov KGZ <78013954+akimovvv@users.noreply.github.com>
2021-06-24 23:17:57 +0600

    Update loader.py

--- loader.py
+++ loader.py
@@ -10,7 +10,7 @@ bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-i18n = setup_middleware(dp)
-_ = i18n.gettext
+# i18n = setup_middleware(dp)
+# _ = i18n.gettext
 
 __all__ = ["bot", "storage", "dp", "db"]
